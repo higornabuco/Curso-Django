364f37a3058ffdb38186c2ce520b2eea485feee2
Higor Nabuco <higornabuco@gmail.com>
2021-07-13 12:19:45 -0300

    Inserindo projeto do curso de Django

---
+++ core/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+def index(request):
+    print(dir(request.user))
+    print(f"User: {request.user}")
+
+    if str(request.user) == 'AnonymousUser':
+        teste = 'Usuário não logado'
+    else:
+        teste = 'Usuário logado'
+    context = {
+        'curso': 'Programnação Web com Django Framework',
+        'outro': 'Django é massa!',
+        'logado': teste
+    }
+    return render(request, 'index.html', context)
+
+
+def contato(request):
+    return render(request, 'contato.html')
