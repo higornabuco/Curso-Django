5b71218d5f957d0990605f064e65c4a09005aacd
Higor Nabuco <higornabuco@gmail.com>
2021-07-22 12:09:22 -0300

    Atualizando Codigo

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.urls.conf import include
 
 from .views import index, contato
 
 
 urlpatterns = [
-    path('', index),
+    path('grappelli/', include ('grappelli.urls')), # grappelli URLS
+    path('index', index),
     path('contato', contato),
 ]
